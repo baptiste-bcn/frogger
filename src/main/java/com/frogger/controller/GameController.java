76e0323454acb602875bd37ab150f912ebba9619
trCVZ <tristan.cvz.pro@gmail.com>
2025-03-26 08:51:49 +0100

    Changes in a new branch

--- src/main/java/com/frogger/controller/GameController.java
+++ src/main/java/com/frogger/controller/GameController.java
@@ -46,57 +46,6 @@ public class GameController {
         this.obstacles = new ArrayList<>();
         this.routes = new ArrayList<>();
 
-        initializeObstacles();
-    }
-
-    private void initializeObstacles() {
-        Random random = new Random();
-
-        // Ajout de routes normales
-        for (int i = 5; i < grille.getHeight() - 3; i += 3) {
-            routes.add(new Route(grille, i, grille.getTileSize()));
-        }
-
-        // Ajout d'obstacles mobiles (voitures)
-        obstacles.add(new Obstacle(grille, 2, 5, 3, 1, Color.RED, false)); // Voiture
-        obstacles.add(new Obstacle(grille, 2, 11, 3, -1, Color.RED, false)); // Voiture
-
-        // Ajout d'une route "eau" avec troncs flottants
-        obstacles.add(new Obstacle(grille, 8, 8, 2, 1, Color.BROWN, false)); // Tronc
-        obstacles.add(new Obstacle(grille, 2, 8, 2, 1, Color.BROWN, false)); // Tronc
-
-        // Génération aléatoire d'arbres
-        int nombreArbres = random.nextInt(15) + 25; // Entre 15 et 25 arbres
-
-        for (int i = 0; i < nombreArbres; i++) {
-            int x, y;
-            boolean positionValide;
-
-            do {
-                x = random.nextInt(grille.getWidth()); // Position aléatoire en X
-                y = random.nextInt(grille.getHeight()); // Position aléatoire en Y
-                positionValide = true;
-
-                for (Route route : routes) {
-                    if (route.getY() == y) {
-                        positionValide = false;
-                        break;
-                    }
-                }
-                for (Obstacle obstacle : obstacles) {
-                    if (obstacle.getX() == x && obstacle.getY() == y) {
-                        positionValide = false;
-                        break;
-                    }
-                    if (y >= grille.getHeight() - 2) {
-                        positionValide = false;
-                        break;
-                    }
-                }
-            } while (!positionValide);
-
-            obstacles.add(new Obstacle(grille, x, y, 1, 0, Color.GREEN, true)); // Arbre statique
-        }
     }
 
     private void handleKeyPress(KeyEvent event) {
@@ -225,7 +174,7 @@ public class GameController {
         stage.setScene(scene);
         stage.show();
 
-        startGameLoop(); // Lancer l'animation des obstacles
+        startGameLoop();
     }
 
     private Button createStyledButton() {
